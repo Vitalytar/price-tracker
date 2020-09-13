02979bd4257f967780548ec7b69f5d8c81dbac0b
Vitalijs Visnakovs <vitalijs.visnakovs@scandiweb.com>
2020-09-13 22:31:51 +0300

    Fixes, structuring

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,12 @@
 <footer class="page-footer font-small blue pt-4 bg-white">
     <div class="container-fluid text-center text-md-left">
         <div class="row">
-            <div class="col-md-6 mt-md-0 mt-3">
-                <h5 class="text-uppercase">Footer Content</h5>
-                <p>Here you can use rows and columns to organize your footer content.</p>
-
+            <div class="col-md-6 mt-md-0 mt-3 contact-form-section">
+                <h5 class="text-uppercase"><?= __('Kontaktforma') ?></h5>
+                <p><?= __('Mēs jums atbildēsim cik ātri vien iespējams.') ?></p>
+                <div class="cf-form trigger-modal">
+                    <?= __('Sazināties') ?>
+                </div>
             </div>
             <hr class="clearfix w-100 d-md-none pb-3">
             <div class="col-md-3 mb-md-0 mb-3">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('pageTitle', 'Price Tracker')</title>
+    <title>@yield('pageTitle', 'TRLPRICE')</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
