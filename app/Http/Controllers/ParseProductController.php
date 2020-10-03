5bf482f7feccc45e4f7d57f048109c925fd6a217
Vitalytar <vitalijs.visnakovs@gmail.com>
2020-10-03 22:33:52 +0300

    Optimized code and removed unnecessary page

--- app/Http/Controllers/ParseProductController.php
+++ app/Http/Controllers/ParseProductController.php
@@ -23,7 +23,8 @@ class ParseProductController extends Controller
 
     const AVAILABLE_WEBSITES = [
         'www.1a.lv',
-        'www.rdveikals.lv'
+        'www.rdveikals.lv',
+        'www.ksenukai.lv'
     ];
 
     const ESCAPE_RUS = [
@@ -31,6 +32,12 @@ class ParseProductController extends Controller
         'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
         'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П',
         'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'
+    ];
+
+    const ESCAPE_LATVIAN = [
+        'Ā' => 'a', 'ā' => 'a', 'Č' => 'c', 'č' => 'c', 'Ē' => 'e', 'ē' => 'e', 'Ģ' => 'g', 'ģ' => 'g', 'Ī' => 'i',
+        'ī' => 'i', 'Ķ' => 'k', 'ķ' => 'k', 'Ļ' => 'l', 'ļ' => 'l', 'Ņ' => 'n', 'ņ' => 'n', 'Š' => 's', 'š' => 's',
+        'Ū' => 'ū', 'ū' => 'u', 'Ž' => 'z', 'ž' => 'z'
     ];
 
     /**
@@ -59,23 +66,31 @@ class ParseProductController extends Controller
     protected $productData = [];
 
     /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
      * ParseProductController constructor.
      *
      * @param Document           $document
      * @param ProductPrice       $productPriceModel
      * @param Product            $productDetailsModel
      * @param UserProductRequest $userRequestedProductModel
+     * @param Request            $request
      */
     public function __construct(
         Document $document,
         ProductPrice $productPriceModel,
         Product $productDetailsModel,
-        UserProductRequest $userRequestedProductModel
+        UserProductRequest $userRequestedProductModel,
+        Request $request
     ) {
         $this->document = $document;
         $this->productPriceModel = $productPriceModel;
         $this->productDetailsModel = $productDetailsModel;
         $this->userRequestedProductModel = $userRequestedProductModel;
+        $this->request = $request;
     }
 
     /**
@@ -87,11 +102,10 @@ class ParseProductController extends Controller
      */
     public function parse(Request $request)
     {
-        // TODO: Check DB table using URL if product already exist and it shouldn't check image and other data
+        $needToCreateProduct = true;
         $productUrl = strtok($request->input('product-url'), '?');
-        $this->productDetailsModel->product_url = $productUrl;
+        $product = $this->checkIfProductExist($productUrl);
         $sourceWeb = parse_url($productUrl, PHP_URL_HOST);
-        $this->productDetailsModel->source_web = $sourceWeb;
 
         if (!in_array($sourceWeb, self::AVAILABLE_WEBSITES)) {
             return redirect()->back()->with(
@@ -103,45 +117,86 @@ class ParseProductController extends Controller
 
         $dom = $this->document->loadHtmlFile($productUrl);
 
-        if ($sourceWeb == 'www.1a.lv') {
-            $this->parseProductData($dom, '.product-righter h1', '.product-gallery-slider__slide__inner img', $sourceWeb);
-            $this->parseAndSaveProductPrice($dom);
-        } else {
-            if ($sourceWeb == 'www.rdveikals.lv') {
-                $this->parseProductData($dom, 'h1', 'img', $sourceWeb);
-                $this->rdParsePrice($dom);
+        if ($product) {
+            $this->productDetailsModel = $product->first();
+
+            if ($sourceWeb == 'www.1a.lv') {
+                $this->parseAndSaveProductPrice($dom);
+            } else {
+                if ($sourceWeb == 'www.rdveikals.lv') {
+                    $this->rdParsePrice($dom);
+                }
+            }
+
+            $this->saveUserRequestData();
+            $needToCreateProduct = false;
+        }
+
+        if ($needToCreateProduct) {
+            $this->productDetailsModel->product_url = $productUrl;
+            $this->productDetailsModel->source_web = $sourceWeb;
+
+            if ($sourceWeb == 'www.1a.lv' || $sourceWeb == 'www.ksenukai.lv') {
+                $this->parseProductData($dom, '.product-righter h1', '.product-gallery-slider__slide__inner img', $sourceWeb);
+                $this->parseAndSaveProductPrice($dom);
+            } else {
+                if ($sourceWeb == 'www.rdveikals.lv') {
+                    $this->parseProductData($dom, 'h1', 'img', $sourceWeb);
+                    $this->rdParsePrice($dom);
+                }
+            }
+
+            $this->saveUserRequestData();
+
+            $this->productData = [
+                'product_name' => $this->productDetailsModel->product_name,
+                'product_image' => $this->productDetailsModel->product_image_url
+            ];
+
+            $prices = $this->productPriceModel->where('product_relation_id', $this->productDetailsModel->id)
+                ->orderBy('created_at', 'desc')
+                ->get();
+
+            foreach ($prices as $price) {
+                $productPrices[] = [
+                    'price' => $price->getAttribute('product_price'),
+                    'currency' => $price->getAttribute('currency'),
+                    'date' => $price->getAttributes()['created_at']
+                ];
             }
         }
 
-        $this->userRequestedProductModel->user_id = ($request->user()) ? $request->user()->id : 0;
-        $this->userRequestedProductModel->requested_product_id = $this->productDetailsModel->id;
-        $this->userRequestedProductModel->save();
-
-        $this->productData = [
-            'product_name' => $this->productDetailsModel->product_name,
-            'product_image' => $this->productDetailsModel->product_image_url
-        ];
-
-        $prices = $this->productPriceModel->where('product_relation_id', $this->productDetailsModel->id)
-            ->orderBy('created_at', 'desc')
-            ->get();
-
-        foreach ($prices as $price) {
-            $productPrices[] = [
-                'price' => $price->getAttribute('product_price'),
-                'currency' => $price->getAttribute('currency'),
-                'date' => $price->getAttributes()['created_at']
-            ];
-        }
-
-        return redirect()->route('product-info')->with(
+        return redirect()->action(
+            'ProductPageController@showProduct',
             [
-                'status' => __('Produkta dati veiksmīgi tika paņemti un saglabāti!'),
-                'main_data' => $this->productData,
-                'price_data' => $productPrices,
-                'source_url' => $this->productDetailsModel->product_url
+                'productName' => strtr(strtolower(str_replace([' ', ',', '/'], '-', $this->productDetailsModel->product_name)), self::ESCAPE_LATVIAN),
+                'productId' => $this->productDetailsModel->id
             ]
         );
+    }
+
+    public function saveUserRequestData()
+    {
+        $this->userRequestedProductModel->user_id = ($this->request->user()) ? $this->request->user()->id : 0;
+        $this->userRequestedProductModel->requested_product_id = $this->productDetailsModel->id;
+        $this->userRequestedProductModel->save();
+    }
+
+    /**
+     * @param $productUrl
+     *
+     * @return false
+     */
+    public function checkIfProductExist($productUrl)
+    {
+        $productExist = false;
+        $product = $this->productDetailsModel->where('product_url', $productUrl)->get();
+
+        if (count($product)) {
+            $productExist = $product;
+        }
+
+        return $productExist;
     }
 
     /**
@@ -225,6 +280,8 @@ class ParseProductController extends Controller
     /**
      * @param $url
      * @param $productName
+     *
+     * @return string
      */
     public function downloadProductImage($url, $productName)
     {
