6a60fb1d111733d1597d4d28f87a16c1834ab725
Vitalytar <vitalijs.visnakovs@gmail.com>
2020-10-13 23:58:35 +0300

    Updated request product page, added instruction and info

--- resources/views/products/check-product-price.blade.php
+++ resources/views/products/check-product-price.blade.php
@@ -12,6 +12,26 @@
         @csrf
         <label for="product-url">Ievadiet produkta linku lai dabūtu cenu izmaiņu grafiku</label>
             <input type="url" name="productUrl" minlength="3" required>
+        <small>Piemērs: https://trlprice.com/links-uz-produktu</small>
         <button type="submit"><?= __('Pārbaudīt cenas vēsturi') ?></button>
     </form>
+    <div class="parse-instruction-block">
+        <h1 class="instruction-title">Kā tas strādā</h1>
+        <div class="instruction-wrapper">
+            <div class="instruction">
+                <ol>
+                    <li>Atrodiet interesējošo produktu vienā no pieejamajiem resursiem</li>
+                    <li>Iekopējiet produkta linku laukā</li>
+                    <li>Nospiediet uz pogu 'Pārbaudīt cenas vēsturi'</li>
+                </ol>
+            </div>
+            <div class="available-resources">
+                <h4>Atbalstītās vietnes</h4>
+                <div class="available-resource">1a</div>
+                <div class="available-resource">RD veikals</div>
+                <div class="available-resource">Dateks</div>
+                <div class="available-resource">Ksenukai</div>
+            </div>
+        </div>
+    </div>
 @endsection
